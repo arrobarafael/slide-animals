91502ef1ff8b3fd6b875e37eee2e98e610b8bcf2
Rafael Pelegrini <arrobarafael@hotmail.com>
2020-05-28 15:33:36 -0300

    Adicionada paginação por slide

--- js/script.js
+++ js/script.js
@@ -4,5 +4,6 @@ const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 console.log(slide);
 slide.addArrow(".prev", ".next");
+slide.addControl();
 // slide.changeSlide(0);
 // slide.activeNextSlide();
